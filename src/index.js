9b67704cecff73baad057cc950fe6267841528d0
achilleleal <achilleleal@gmail.com>
2020-11-20 18:18:29 -0400

    made game executable by user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import _ from "lodash";
+import gameOfLife from './gameOfLife'
 
 const game = document.getElementById('game');
+const startBtn = document.getElementById('start-btn');
+const counter = document.getElementById('counter');
 
 let grid = [
 	[0,0,0,0,0,0,0,0,0,0],
@@ -11,118 +13,35 @@ let grid = [
     [0,0,0,0,0,0,0,0,0,0]
 ];
 
-const gameOfLife = () => {
+function generateGrid() {
+    for (let y = 0; y < grid.length; y++) {
 
-    try {
-
-        let newGen;
-        let gen = 0;
-
-        const loop = setInterval(() => {
-            console.log(gen)
-
-            game.innerHTML = '';
-            newGen = _.cloneDeep(grid); 
-
-            for (let y = 0; y < grid.length; y++) {
-
-                const row = document.createElement('div');
-                row.classList.add('row');
-
-                for (let x = 0; x < grid[y].length; x++) {
-
-                    const cell = document.createElement('div');
-                    cell.classList.add('cell');
-
-                    const gridCell = grid[y][x]
-                    const totalNeighbours = upperCells(x,y) + bottomCells(x,y) + lineCells(x,y)
-
-                    if (gridCell) {
-                        if (totalNeighbours < 2 || totalNeighbours > 3) {
-                            cell.classList.add('dead');
-                            newGen[y][x] = 0; // Cell dies
-                        } else {// Else cell lives
-                            cell.classList.add('alive');
-                        }
-                    } else {
-                        if (totalNeighbours === 3) {
-                            cell.classList.add('alive');
-                            newGen[y][x] = 1; // Cell is born!
-                        } else {
-                            cell.classList.add('dead');
-                        }
-                    }
-
-                    row.appendChild(cell);
-                }
-                game.appendChild(row) 
-            }
-
-            if (_.isEqual(grid, newGen)) {
-                clearInterval(loop)
-            } else {
-                gen++;
-                grid = newGen;
-            }
-
-        }, 500)
-
-    } catch (err) {
-        console.log('Error: ', err)
+        const row = document.createElement('div');
+        row.classList.add('row');
+    
+        for (let x = 0; x < grid[y].length; x++) {
+    
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener('click', () => cell.classList.toggle('alive'))
+    
+            row.appendChild(cell);
+        }
+        game.appendChild(row) 
     }
 }
 
-gameOfLife()
+function run() {
 
-// Counts the two adjacent cells to the current one
-function lineCells(x,y) {
+    startBtn.classList.add('running');
+    counter.innerHTML = 0;
 
-    const prevCell = x > 0 
-                        ? grid[y][x-1] 
-                        : 0;
+    const end = () => startBtn.classList.remove('running');
+    const interval = document.querySelector('input').value;
 
-    const nextCell = x < grid[y].length - 1 
-                      ? grid[y][x+1] 
-                      : 0;
-
-    return prevCell + nextCell
+    gameOfLife(interval, grid, game, counter, end);
 }
 
-// Counts the three upper neighbour cells of the current one
-function upperCells(x,y) {
-    if (grid[y-1]) {
+startBtn.addEventListener('click', run);
 
-        const uCell = grid[y-1][x];
-
-        const previousUCell = x > 0 
-                                ? grid[y-1][x-1] 
-                                : 0;
-
-        const nextUCell = x < grid[y-1].length - 1 
-                            ? grid[y-1][x+1] 
-                            : 0;
-
-        return uCell + previousUCell + nextUCell;
-    }
-    return 0;
-}
-
-// Counts the three lower neighbour cells of the current one
-function bottomCells(x,y) {
-    if (grid[y+1]) {
-
-        const bCell = grid[y+1][x];
-
-        const previousBCell = x > 0 
-                                ? grid[y+1][x-1] 
-                                : 0;
-                                
-        const nextBCell = x < grid[y+1].length - 1 
-                            ? grid[y+1][x+1] 
-                            : 0;
-
-        return bCell + previousBCell + nextBCell;
-    }
-    return 0;
-}
-
+generateGrid()

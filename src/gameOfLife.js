0f090e9a37af8b56243ec2437194f92d3b6b6644
achilleleal <achilleleal@gmail.com>
2020-11-23 12:16:52 -0400

    added comments

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -13,6 +13,9 @@ export default class GameOfLife {
 
         const rows = Array.from(document.getElementsByClassName('row'));
 
+        // Create a binary representation of each cell
+        // 0 = cell is dead
+        // 1 = cell is alive
         let cellsRef = rows.map(
             row => Array.from(row.children).map(
                 cell => cell.className === 'cell alive' ? 1 : 0
@@ -21,6 +24,8 @@ export default class GameOfLife {
 
         this.interval = setInterval(() => { 
     
+            // Deep clone the reference to be able to mutate it on the fly without affecting cell replication outcome
+            // The clone will be used to loop through each cell and check its neighbours
             const cells = _.cloneDeep(cellsRef)
     
             cells.forEach(
@@ -30,23 +35,32 @@ export default class GameOfLife {
                             const neighbours = lineCells(cells, x, y) + upperCells(cells, x, y) + bottomCells(cells, x, y)
 
                             if ( cell && neighbours < 2 || neighbours > 3 ) {
-                                rows[y].children[x].classList.remove('alive');
-                                cellsRef[y][x] = 0;
+                                // Cell dies by underpopulation or overpopulation
+                                rows[y].children[x].classList.remove('alive'); // Update cell in display grid
+                                cellsRef[y][x] = 0; // Update cell status in ref
 
                             } else if (neighbours === 3) {
+                                // Cell is born when it has exactly 3 neighbours
                                 rows[y].children[x].classList.add('alive');
                                 cellsRef[y][x] = 1;
                             }
+                            // else the status of the cell doesn't change
                         }
                     )
             })
     
             if (_.isEqual(cells, cellsRef)) {
+                /*
+                If the grid hasn't changed, it means that the game has softlocked.
+                It won't change anymore unless the player changes the cell arrangement, so further looping is unneeded.
+
+                Stop the game to let the player change the arrangements.
+                */
                 this.stop()
-                callback()
+                callback() // Effects to be executed when game is over (change a button's bg, etc.)
             } else {
                 this.gens++;
-                counter.innerHTML = this.gens;
+                counter.innerHTML = this.gens; // Update game counter
             }
     
         }, time)
@@ -66,7 +80,7 @@ export default class GameOfLife {
 
 
 
-// Counts the two adjacent cells to the current one
+// Counts the left and right neighbour cells of the current cell
 function lineCells(cells, x, y) {
 
     const prevCell = cells[y][x-1] || 0;
@@ -76,7 +90,7 @@ function lineCells(cells, x, y) {
     return prevCell + nextCell
 }
 
-// Counts the three upper neighbour cells of the current one
+// Counts the three neighbour cells in the row above of the current cell  
 function upperCells(cells, x, y) {
 
     if (y > 0) {
@@ -93,7 +107,7 @@ function upperCells(cells, x, y) {
     return 0;
 }
 
-// Counts the three lower neighbour cells of the current one
+// Counts the three neighbour cells in the row below of the current cell  
 function bottomCells(cells, x, y) {
 
     if (y < cells.length-1) {
